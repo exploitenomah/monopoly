cba414845e412c195cc5b9a09163a4363da229f7
exploitenomah <iksploiting@gmail.com>
2023-10-25 16:22:15 +0100

    create go to jail component

---
+++ app/_components/Spaces/GoToJail.tsx
@@ -0,0 +1,9 @@
+import GoToJailTile from "../Tiles/GoToJail"
+
+export default function GoToJail() {
+  return (
+    <div  className="absolute w-[12.5%] h-[12.5%] border-black border-l-solid border-b-solid border-l-[1px] border-b-[1px] lg:border-l-[3px] lg:border-b-[3px] top-0 right-0">
+      <GoToJailTile />
+    </div>
+  )
+}
