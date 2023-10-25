cea823a2f210d74544779961ddcd3a27f7d27708
exploitenomah <iksploiting@gmail.com>
2023-10-25 14:37:14 +0100

    create housing property class

---
+++ app/_classes/HousingProperty.ts
@@ -0,0 +1,32 @@
+import Property from "./Property"
+
+export interface HousingPropertyRent {
+  default: number
+  oneHouse: number
+  twoHouses: number
+  threeHouses: number
+  fourHouses: number
+  hotel: number
+}
+
+class HousingProperty extends Property {
+  public housesCount: number = 0
+  public hotelsCount: number = 0
+  public color?: string
+  public rent: HousingPropertyRent
+  public pricePerHouse: number
+
+  constructor(
+    name: string,
+    price: number,
+    rent: HousingPropertyRent,
+    pricePerHouse: number,
+    color?: string
+  ) {
+    super(name, price)
+    this.color = color
+    this.rent = rent
+    this.pricePerHouse = pricePerHouse
+  }
+}
+export default HousingProperty
