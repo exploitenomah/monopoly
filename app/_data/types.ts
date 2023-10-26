31f4a0e47d82ffe5ab6195e3f4c38d7cb2b512f8
exploitenomah <iksploiting@gmail.com>
2023-10-26 14:15:45 +0100

    add types

---
+++ app/_data/types.ts
@@ -0,0 +1,10 @@
+import HousingProperty from "@/app/_classes/HousingProperty"
+import StationProperty from "@/app/_classes/StationProperty"
+import UtilityProperty from "@/app/_classes/HousingProperty"
+import Space from "@/app/_classes/Space"
+
+export type TileType =
+  HousingProperty
+  |  StationProperty
+  |  UtilityProperty
+  |  Space
