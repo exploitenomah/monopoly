341154e8a99dd6fee932b4fd667ca94948e6e097
exploitenomah <iksploiting@gmail.com>
2023-10-28 19:47:38 +0100

    add static revive method

--- app/_classes/HousingProperty.ts
+++ app/_classes/HousingProperty.ts
@@ -10,7 +10,7 @@ export interface HousingPropertyRent {
 }
 
 class HousingProperty extends Property {
-  public type = 'HOUSING'
+  public type = "HOUSING"
   public housesCount: number = 0
   public hotelsCount: number = 0
   public color?: string
@@ -18,16 +18,56 @@ class HousingProperty extends Property {
   public pricePerHouse: number
 
   constructor(
+    id:string,
     name: string,
     price: number,
     rent: HousingPropertyRent,
     pricePerHouse: number,
     color?: string
   ) {
-    super(name, price)
+    super(id, name, price)
     this.color = color
     this.rent = rent
     this.pricePerHouse = pricePerHouse
+  }
+
+  public static revive(objectLikeHousingProperty: HousingProperty) {
+    const {
+      housesCount,
+      hotelsCount,
+      color,
+      rent,
+      pricePerHouse,
+      name,
+      price,
+      isOwned,
+      owner,
+      isMortgaged,
+      mortgageValue,
+      redemptionValue,
+      id
+    } = objectLikeHousingProperty
+    const revivedHousingProperty = new HousingProperty(
+      id,
+      name,
+      price,
+      rent,
+      pricePerHouse,
+      color
+    )
+    revivedHousingProperty.housesCount = housesCount
+    revivedHousingProperty.hotelsCount = hotelsCount
+    revivedHousingProperty.color = color
+    revivedHousingProperty.rent = rent
+    revivedHousingProperty.pricePerHouse = pricePerHouse
+    revivedHousingProperty.name = name
+    revivedHousingProperty.price = price
+    revivedHousingProperty.isOwned = isOwned
+    revivedHousingProperty.owner = owner
+    revivedHousingProperty.isMortgaged = isMortgaged
+    revivedHousingProperty.mortgageValue = mortgageValue
+    revivedHousingProperty.redemptionValue = redemptionValue
+    return revivedHousingProperty
   }
 }
 export default HousingProperty
