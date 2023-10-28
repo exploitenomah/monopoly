01256239450ef4cc586ac789aa45a36e08f3c588
exploitenomah <iksploiting@gmail.com>
2023-10-28 19:48:04 +0100

    add static revive method

--- app/_classes/Property.ts
+++ app/_classes/Property.ts
@@ -1,6 +1,6 @@
 import Player from "./Player"
 
-const Property = class {
+class  Property {
   public isOwned: boolean = false
   public owner: null | Player = null
   public price: number
@@ -8,12 +8,40 @@ const Property = class {
   public mortgageValue: number
   public redemptionValue: number
   public name: string
+  public id: string
 
-  constructor(name: string, price: number) {
+  constructor(id:string, name: string, price: number) {
+    this.id = id
     this.name = name
     this.price = price
     this.mortgageValue = this.price / 2
     this.redemptionValue = this.mortgageValue + 0.1 * this.mortgageValue
+  }
+
+  public static revive(objectLikeProperty: Property) {
+    const {
+      name,
+      price,
+      isOwned,
+      owner,
+      isMortgaged,
+      mortgageValue,
+      redemptionValue,
+      id,
+    } = objectLikeProperty
+    const revivedProperty = new Property(
+      id,
+      name,
+      price
+    )
+    revivedProperty.name= name
+    revivedProperty.price= price
+    revivedProperty.isOwned= isOwned
+    revivedProperty.owner= owner 
+    revivedProperty.isMortgaged=  isMortgaged
+    revivedProperty.mortgageValue= mortgageValue
+    revivedProperty.redemptionValue= redemptionValue
+    return revivedProperty
   }
 }
 
