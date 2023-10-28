a6b2252cda73192e830ffc876f8654565f86e5f5
exploitenomah <iksploiting@gmail.com>
2023-10-28 19:52:44 +0100

    display game cards

--- app/_components/BoardCenter.tsx
+++ app/_components/BoardCenter.tsx
@@ -1,3 +1,15 @@
-export default function BoardCenter() {
-  return <></>
+import BoardGame from "@/app/_classes/BoardGame"
+import CommunityChestContainer from "./GameCards/CommunityChestContainer"
+import ChanceContainer from "./GameCards/ChanceContainer"
+
+export default function BoardCenter({ game }: { game: BoardGame }) {
+  const communityChestCards = game.communityChestCards
+  const chanceCards = game.chanceCards
+
+  return (
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[76%] h-[76%] ">
+      <CommunityChestContainer cards={communityChestCards} />
+      <ChanceContainer cards={chanceCards} />
+    </div>
+  )
 }
