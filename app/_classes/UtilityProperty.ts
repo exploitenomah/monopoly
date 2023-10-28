fb165e5636d72c5f6087f670f2da644dff407279
exploitenomah <iksploiting@gmail.com>
2023-10-28 19:49:13 +0100

    add static revive method

--- app/_classes/UtilityProperty.ts
+++ app/_classes/UtilityProperty.ts
@@ -11,14 +11,43 @@ class UtilityProperty extends Property {
     return value * 10
   }
   constructor(
+    id: string,
     name: string,
     price: number,
-    rent: UtilityPropertyRent,
     category: string
   ) {
-    super(name, price)
-    this.rent = rent.default
+    super(id, name, price)
     this.category = category
+  }
+
+  public static revive(objectLikeUtilityProperty: UtilityProperty) {
+    const {
+      rent,
+      name,
+      price,
+      isOwned,
+      owner,
+      isMortgaged,
+      mortgageValue,
+      redemptionValue,
+      category,
+      id
+    } = objectLikeUtilityProperty
+    const revivedUtilityProperty = new UtilityProperty(
+      id,
+      name,
+      price,
+      category
+    )
+    revivedUtilityProperty.rent = rent
+    revivedUtilityProperty.name = name
+    revivedUtilityProperty.price = price
+    revivedUtilityProperty.isOwned = isOwned
+    revivedUtilityProperty.owner = owner
+    revivedUtilityProperty.isMortgaged = isMortgaged
+    revivedUtilityProperty.mortgageValue = mortgageValue
+    revivedUtilityProperty.redemptionValue = redemptionValue
+    return revivedUtilityProperty
   }
 }
 
