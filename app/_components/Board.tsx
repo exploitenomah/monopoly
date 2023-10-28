8a029e3d91e16f7e65c3c5b9100d7baed670602e
exploitenomah <iksploiting@gmail.com>
2023-10-28 19:52:22 +0100

    display unique boards for different games

--- app/_components/Board.tsx
+++ app/_components/Board.tsx
@@ -6,21 +6,23 @@ import Go from "./Spaces/Go"
 import Jail from "./Spaces/Jail"
 import FreeParking from "./Spaces/FreeParking"
 import GoToJail from "./Spaces/GoToJail"
+import BoardGame from "@/app/_classes/BoardGame"
+import BoardCenter from "./BoardCenter"
 
-export default function Board() {
+export default function Board({ game }: { game: BoardGame | null }) {
+  if (!game) return "asdfakjsdfj"
+
   return (
-    <div
-      // style={{ transform: "translateY(-12%) rotateX(35deg)" }}
-      className="board bg-primary-default h-[100vw] w-[100vw] sm:h-[85vw] sm:w-[85vw] md:h-[80vw] md:w-[80vw] lg:h-[100vh] lg:w-[100vh] relative"
-    >
-      <LineOne />
+    <div className="board bg-primary-default h-[100vw] w-[100vw] sm:h-[85vw] sm:w-[85vw] md:h-[80vw] md:w-[80vw] lg:h-[100vh] lg:w-[100vh] relative">
+      <LineOne tiles={game.properties.LineOne} />
       <Jail />
-      <LineTwo />
+      <LineTwo tiles={game.properties.LineTwo} />
       <FreeParking />
-      <LineThree />
+      <LineThree tiles={game.properties.LineThree} />
       <GoToJail />
-      <LineFour />
+      <LineFour tiles={game.properties.LineFour} />
       <Go />
+      <BoardCenter game={game} />
     </div>
   )
 }
