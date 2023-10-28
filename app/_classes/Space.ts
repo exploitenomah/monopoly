ee8c10c179e4b3cf9c2c42335614d844971be3b5
exploitenomah <iksploiting@gmail.com>
2023-10-28 19:48:20 +0100

    add static revive method

--- app/_classes/Space.ts
+++ app/_classes/Space.ts
@@ -9,8 +9,14 @@ type SpaceType =
   | "SUPER-TAX"
 
 export default class Space {
-  type: SpaceType
-  constructor(type: SpaceType) {
+  public type: SpaceType
+  public id: string
+  constructor(id: string, type: SpaceType) {
     this.type = type
+    this.id = id
+  }
+  public static revive(objectLikeSpace: Space) {
+    const { id , type } = objectLikeSpace
+    return new Space(id, type)
   }
 }
