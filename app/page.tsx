b2fed028d05397647ea476d529e7ece2d91d398b
exploitenomah <iksploiting@gmail.com>
2023-10-28 19:56:57 +0100

    update default view

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,33 @@
-import Board from "./_components/Board"
+"use client"
+import useManageGames from "./_hooks/useManageGames"
+import Game from "./_components/BoardGame"
+import HomeDefaultDisplay from "./_components/Home"
+import useManageKeyInLocalStorage from "./_hooks/useManageKeyInLocalStorage"
 
-export default function Home() {
+function Home() {
+  const { games, createNewGame } = useManageGames("MONOPOLY_GAMES", [])
+  const [currentGameId, updateCurrentGameId, clearCurrentGameId] =
+    useManageKeyInLocalStorage("CURRENT_GAME_ID", null)
+  /// existing game ? choose game to play : new game
+
+  if (!currentGameId)
+    return (
+      <HomeDefaultDisplay
+        games={games}
+        createNewGame={(newGame) => {
+          createNewGame(newGame)
+          updateCurrentGameId(newGame.id)
+        }}
+        updateCurrentGameId={updateCurrentGameId}
+      />
+    )
+  return <Game gameDetails={games.filter((it) => it.id === currentGameId)[0]} />
+}
+
+export default function HomeWrapper() {
   return (
-    <main
-      style={{ perspective: "1000px" }}
-      className="w-screen h-screen flex justify-center items-center"
-    >
-      <Board />
+    <main className="w-screen h-screen flex justify-center items-center">
+      <Home />
     </main>
   )
 }
