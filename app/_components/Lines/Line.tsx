7edb587879dea54fa0649eabb9233524043fa9dc
exploitenomah <iksploiting@gmail.com>
2023-10-28 19:53:26 +0100

    updates

--- app/_components/Lines/Line.tsx
+++ app/_components/Lines/Line.tsx
@@ -1,12 +1,11 @@
 import Tile from "../Tiles/Tile"
+import { TileContent } from "@/app/_classes/Tile"
 
-import TileClass from "@/app/_classes/Tile"
-
-export default function Line({ tiles = [] }: { tiles: TileClass[] }) {
+export default function Line({ tiles = [] }: { tiles: TileContent[] }) {
   return (
     <div className="flex flex-row-reverse w-full h-full">
       {tiles.map((tile, idx) => (
-        <Tile key={idx} content={tile.content} tileId={tile.id} />
+        <Tile key={tile.id} content={tile} tileId={tile.id} />
       ))}
     </div>
   )
