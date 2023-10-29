1ccd78ac56d620d0e65ec9d64eec9c648ef749e7
exploitenomah <iksploiting@gmail.com>
2023-10-29 01:53:37 +0100

    refactor

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,7 @@ function Home() {
   const [currentGameId, updateCurrentGameId, clearCurrentGameId] =
     useManageKeyInLocalStorage("CURRENT_GAME_ID", null)
   const [loading, setLoading] = useState(true)
+  const [showGameCreationForm, setShowGameCreationForm] = useState(false)
 
   useEffect(() => {
     const loadingTimeout = setTimeout(() => {
@@ -31,15 +32,25 @@ function Home() {
           createNewGame(newGame)
           updateCurrentGameId(newGame.id)
         }}
+        showGameCreationFormByDefault={showGameCreationForm}
         updateCurrentGameId={updateCurrentGameId}
       />
     )
-  return <Game gameDetails={games.filter((it) => it.id === currentGameId)[0]} />
+  return (
+    <Game  
+      createNewGame={() => {
+        clearCurrentGameId()
+        setShowGameCreationForm(true)
+      }}
+      clearCurrentGameId={clearCurrentGameId} 
+      gameDetails={games.filter((it) => it.id === currentGameId)[0]}
+    />
+  )
 }
 
 export default function HomeWrapper() {
   return (
-    <main className="w-screen h-screen flex justify-center items-center">
+    <main className="w-screen h-screen flex justify-center items-center bg-primary-dark">
       <Home />
     </main>
   )
