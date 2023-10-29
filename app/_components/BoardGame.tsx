ecffbaf5b66f48471d2d31224e638be5580ec8d9
exploitenomah <iksploiting@gmail.com>
2023-10-29 01:51:02 +0100

    add game authentication

--- app/_components/BoardGame.tsx
+++ app/_components/BoardGame.tsx
@@ -1,14 +1,23 @@
 import Board from "./Board"
 import useSingleGameManager from "../_hooks/useSingleGameManager"
 import { GameDetails } from "../types"
+import AuthorizationForm from "./AuthorizationForm"
 
 export default function GameBoard({
   gameDetails,
+  clearCurrentGameId,
+  createNewGame
 }: {
   gameDetails: GameDetails
+  clearCurrentGameId: () => void
+  createNewGame: () => void
 }) {
-  const { game } = useSingleGameManager(gameDetails)
+  const { game, isAuthorized, authorizeToPlay } =
+    useSingleGameManager(gameDetails)
 
+  if (!isAuthorized)
+    return <AuthorizationForm verifyPassword={authorizeToPlay} gameDetails={gameDetails} resetToDefaultView={clearCurrentGameId} createNewGame={createNewGame} />
+    
   return (
     <div style={{ perspective: "1000px" }}>
       <Board game={game} />
