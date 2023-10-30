797f9ecf77443f27544f06295610796b261631be
exploitenomah <iksploiting@gmail.com>
2023-10-30 10:38:01 +0100

    render board tiles

--- app/_components/Board.tsx
+++ app/_components/Board.tsx
@@ -10,18 +10,18 @@ import BoardGame from "@/app/_classes/BoardGame"
 import BoardCenter from "./BoardCenter"
 
 export default function Board({ game }: { game: BoardGame | null }) {
-  if (!game) return "asdfakjsdfj"
 
+  if (!game) return <></>
   return (
     <div className="board bg-primary-default h-[100vw] w-[100vw] sm:h-[85vw] sm:w-[85vw] md:h-[80vw] md:w-[80vw] lg:h-[100vh] lg:w-[100vh] relative">
       <LineOne tiles={game.properties.LineOne} />
-      <Jail />
+      <Jail jail={game.Jail} />
       <LineTwo tiles={game.properties.LineTwo} />
-      <FreeParking />
+      <FreeParking freeParking={game.FreeParking} />
       <LineThree tiles={game.properties.LineThree} />
-      <GoToJail />
+      <GoToJail goToJail={game.GoToJail} />
       <LineFour tiles={game.properties.LineFour} />
-      <Go />
+      <Go go={game.Go} />
       <BoardCenter game={game} />
     </div>
   )
