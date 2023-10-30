eb6a1fecf2c4eb9be8cbbe656caa3540fbca6414
exploitenomah <iksploiting@gmail.com>
2023-10-30 03:59:20 +0100

    conditional rendering

--- app/_components/BoardGame.tsx
+++ app/_components/BoardGame.tsx
@@ -2,22 +2,40 @@ import Board from "./Board"
 import useSingleGameManager from "../_hooks/useSingleGameManager"
 import { GameDetails } from "../types"
 import AuthorizationForm from "./AuthorizationForm"
+import InitializationForm from "./InitializationForm"
 
 export default function GameBoard({
   gameDetails,
   clearCurrentGameId,
-  createNewGame
+  createNewGame,
 }: {
   gameDetails: GameDetails
   clearCurrentGameId: () => void
   createNewGame: () => void
 }) {
-  const { game, isAuthorized, authorizeToPlay } =
+  const { game, isAuthorized, authorizeToPlay, initializeGame } =
     useSingleGameManager(gameDetails)
 
+  if (!game) return <></>
+
   if (!isAuthorized)
-    return <AuthorizationForm verifyPassword={authorizeToPlay} gameDetails={gameDetails} resetToDefaultView={clearCurrentGameId} createNewGame={createNewGame} />
-    
+    return (
+      <AuthorizationForm
+        verifyPassword={authorizeToPlay}
+        gameDetails={gameDetails}
+        resetToDefaultView={clearCurrentGameId}
+        createNewGame={createNewGame}
+      />
+    )
+
+  if (game?.isInitialized === false)
+    return (
+      <InitializationForm
+        finalize={initializeGame}
+        totalPlayers={game?.players.length}
+      />
+    )
+
   return (
     <div style={{ perspective: "1000px" }}>
       <Board game={game} />
