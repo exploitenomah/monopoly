e306bdd2e9a0099699bf9492043861cdb501f416
exploitenomah <iksploiting@gmail.com>
2023-10-30 04:03:55 +0100

    refactor

--- app/types.ts
+++ app/types.ts
@@ -1,7 +1,20 @@
-
 export interface GameDetails {
   name: string
   id: string
   password: string
   totalPlayers: number
+}
+export type PlayerColor =
+  | "red"
+  | "orange"
+  | "yellow"
+  | "green"
+  | "blue"
+  |"indigo"
+  | "violet"
+
+export type PlayerDetail = {
+  name: string
+  color: PlayerColor
+  rollValue: number
 }
