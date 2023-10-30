0a1cef81888cc4ee8ae709882a9e4cd0cd27401b
exploitenomah <iksploiting@gmail.com>
2023-10-30 10:45:46 +0100

    rename

--- app/_data/stations/CHISCO.ts
+++
@@ -1,9 +0,0 @@
-import StationProperty from "@/app/_classes/StationProperty"
-
-const CHISCOBusStation = new StationProperty(crypto.randomUUID(),"CHISCO", 200, {
-  oneStationOwned: 25,
-  twoStationsOwned: 50,
-   threeStationsOwned: 100,
-  fourStationsOwned: 200,
-})
-export default CHISCOBusStation
