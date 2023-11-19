954fe5ec99e7ac9b568ab92589dac5672ffe9154
exploitenomah <iksploiting@gmail.com>
2023-11-19 17:18:18 +0100

    update props for functionality

--- app/_components/GameCards/CardsContainer.tsx
+++ app/_components/GameCards/CardsContainer.tsx
@@ -1,8 +1,17 @@
 import { CSSProperties, ReactNode } from "react"
 
-export default function CardContainer({ style, children }: { style?: CSSProperties, children: ReactNode | ReactNode[] }) {
+export default function CardContainer({
+  style,
+  children,
+}: {
+  style?: CSSProperties
+  children: ReactNode | ReactNode[]
+}) {
   return (
-    <div style={style} className="z-10 absolute w-[35%] h-[20%] border-dashed border-2 border-primary-dark">
+    <div
+      style={style}
+      className="z-10 absolute w-[35%] h-[20%] border-dashed border-2 border-primary-dark"
+    >
       {children}
     </div>
   )
