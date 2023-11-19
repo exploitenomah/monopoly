63471940e4e9b3a5e5c19fff809f4cc5f107d7c7
exploitenomah <iksploiting@gmail.com>
2023-11-19 17:28:08 +0100

    update props and style

--- app/_components/Tiles/ContentsHolder.tsx
+++ app/_components/Tiles/ContentsHolder.tsx
@@ -2,12 +2,21 @@ import Player from "@/app/_classes/Player"
 import LocationToken from "../LocationToken"
 import { PlayerColor } from "../../types"
 
-export default function ContentsHolder({ contents, position }: { contents: Player[], position: number }) {
-  // console.log(contents)
+export default function ContentsHolder({
+  contents,
+  position,
+}: {
+  contents: Player[]
+  position: number
+}) {
   return (
-    <div className="absolute inset-0 z-10 bg-transparent flex justify-around flex-wrap">
+    <div className="absolute inset-0 z-[99999] bg-transparent flex justify-around flex-wrap">
       {contents.map((content) => (
-        <LocationToken position={position} key={content.id} color={content.color as PlayerColor} />
+        <LocationToken
+          position={position}
+          key={content.id}
+          color={content.color as PlayerColor}
+        />
       ))}
     </div>
   )
