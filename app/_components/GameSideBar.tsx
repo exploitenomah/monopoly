6ae7161ce495a66d9a647ca2b3866bbbc528c069
exploitenomah <iksploiting@gmail.com>
2023-12-22 15:06:46 +0100

    fix styles

--- app/_components/GameSideBar.tsx
+++ app/_components/GameSideBar.tsx
@@ -1,11 +1,14 @@
-import { useRef } from "react"
+import { useRef, ReactNode } from "react"
 import useOutsideClick from "../_hooks/useOutsideClick"
+
 export default function GameSideBar({
   show,
   close,
+  children
 }: {
   show: boolean
   close: () => void
+  children: ReactNode | ReactNode[]
 }) {
   const sideBarRef = useRef<HTMLDivElement | null>(null)
   useOutsideClick(sideBarRef, () => close())
@@ -16,7 +19,9 @@ export default function GameSideBar({
         show
           ? "translate-x-0 shadow-[-19px_20px_20px_20px_#00000024]"
           : "translate-x-[100%]"
-      } right-0 top-0 fixed h-screen w-[90vw] max-w-[450px] bg-primary-default z-30 duration-300 transition-transform`}
-    ></div>
+      } right-0 top-0 fixed h-screen overflow-auto w-[90vw] max-w-[85%] bg-primary-default z-30 duration-300 transition-transform`}
+    >
+      {children}
+    </div>
   )
 }
