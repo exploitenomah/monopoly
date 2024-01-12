01c8634539d763fbb81011fe053a85f4f507967e
exploitenomah <iksploiting@gmail.com>
2024-01-12 10:45:12 +0100

    refactor

--- app/_hooks/useHandleNotifications.ts
+++ app/_hooks/useHandleNotifications.ts
@@ -1,17 +1,24 @@
 import { useState, useEffect } from "react"
 import BoardGame from "../_classes/BoardGame"
+import Player from "../_classes/Player"
 
 export default function useHandleNotification(game: BoardGame | null) {
   const [notification, setNotification] = useState<string>("")
+  const [playerInTurn, setPlayerInTurn] = useState<Player | undefined>(undefined)
 
   useEffect(() => {
-    setNotification(
-      `${
-        game?.players.filter((it) => it.turn === game?.currentTurn)[0]?.name
-      }'s turn`
-    )
+    const currentPlayer = game?.players.filter((it) => it.turn === game?.currentTurn)[0]
+    if(currentPlayer){
+      setPlayerInTurn(currentPlayer)
+      setNotification(
+        `${
+          currentPlayer?.name
+        }'s turn`
+      )
+    }
   }, [game])
   return {
     notification,
+    playerInTurn
   }
 }
