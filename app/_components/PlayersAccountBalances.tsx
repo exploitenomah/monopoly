ff10a9849992b0402b6f5a7af1711e33344283da
exploitenomah <iksploiting@gmail.com>
2024-01-12 10:44:51 +0100

    refactor

--- app/_components/PlayersAccountBalances.tsx
+++ app/_components/PlayersAccountBalances.tsx
@@ -1,9 +1,8 @@
 import BoardGame from "../_classes/BoardGame"
-import { useMemo, useState } from "react"
+import { useMemo } from "react"
 import PlayerAccountBalance from "./PlayerAccountBalance"
 
 export default function PlayersAccountBalances({ game }: { game: BoardGame | null }) {
-  const [showAccountBalances, setShowAccountBalances] = useState(false)
 
   const accountBalances = useMemo(() => (game?.players?.map((player) => (
     <PlayerAccountBalance
@@ -16,12 +15,9 @@ export default function PlayersAccountBalances({ game }: { game: BoardGame | nul
   if (!game) return null
   return (
     <div className="w-full p-4">
-      <button className="capitalize bg-primary-dark/90 text-primary-default rounded-lg px-4 py-3 font-medium text-lg" onClick={() => setShowAccountBalances(prev => !prev)}>{showAccountBalances ? "hide" : "show"} account balances</button>
-      {showAccountBalances &&
-        <ul className="px-4 py-6 overflow-x-auto flex flex-wap gap-5 sticky top-0">
-          {accountBalances}
-        </ul>
-      }
+      <ul className="px-4 py-6 overflow-x-auto flex flex-wrap gap-5 sticky top-0">
+        {accountBalances}
+      </ul>
     </div>
   )
 }
