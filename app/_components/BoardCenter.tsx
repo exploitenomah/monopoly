7824e2bd136bc5041caf736aa0fc0d5bee1cafb7
exploitenomah <iksploiting@gmail.com>
2024-01-18 20:51:20 +0100

    enable toast for community chest and chance actions

--- app/_components/BoardCenter.tsx
+++ app/_components/BoardCenter.tsx
@@ -19,33 +19,35 @@ export default function BoardCenter({
   return (
     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[76%] h-[76%] z-[300]">
       <CommunityChestContainer
+        game={game}
+        currentPlayer={currentPlayer}
         currentCard={game.currentChestCard}
         cards={game?.communityChestCards || []}
-        currentPlayerId={currentPlayer?.id}
-      />
+        />
       <Image
         src="/image-files/monopoly-logo.svg"
         alt="monopoly"
         width={300}
         height={100}
         className="z-0 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rotate-[-45deg] w-[80%] h-auto"
-      />
+        />
       <div
         className={`${
           !game.currentChestCard && !game.currentChanceCard ? "z-[20]" : "z-[1]"
         } absolute w-full h-full flex items-center justify-center`}
-      >
+        >
         <GameDiceContainer
           diceDisabled={(game.currentChestCard || game.currentChanceCard) ? true : false}
           advanceCurrentPlayer={advanceCurrentPlayer}
           currentPlayer={currentPlayer}
           game={game}
-        />
+          />
       </div>
       <ChanceContainer
         currentCard={game.currentChanceCard}
+        currentPlayer={currentPlayer}
         cards={game?.chanceCards || []}
-        currentPlayerId={currentPlayer?.id}
+        game={game}
       />
     </div>
   )
